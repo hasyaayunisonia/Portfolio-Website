99d0a99fccb3240c901f30b206ebcf4ed8c32151
hasya <hasya.ayuni.tif20@polban.ac.id>
2025-10-28 23:07:35 +0700

    experiences section

--- src/app/section/ExperiencesSection.jsx
+++ src/app/section/ExperiencesSection.jsx
@@ -21,7 +21,7 @@ const experiences = [
   },
 ];
 
-const ExperienceSection = () => {
+const ExperiencesSection = () => {
   return (
     <section>
       <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
@@ -52,4 +52,4 @@ const ExperienceSection = () => {
     </section>
   );
 };
-export default ExperienceSection;
+export default ExperiencesSection;
