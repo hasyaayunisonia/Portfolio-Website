2dd22b619394c10c71762315b3248f498a991e6d
hasya <hasya.ayuni.tif20@polban.ac.id>
2025-11-12 21:30:13 +0700

    adjustment and remove unuse import

--- src/app/section/AboutSection.jsx
+++ src/app/section/AboutSection.jsx
@@ -1,7 +1,5 @@
 "use client";
 import React, { useTransition, useState } from "react";
-import Image from "next/image";
-import adaKeyImage from "@/app/images/adawithkey.jpg";
 import TabButton from "../components/TabButton";
 
 const TAB_DATA = [
@@ -15,8 +13,8 @@ const TAB_DATA = [
     id: "certifications",
     content: (
       <ul className="list-disc pl-2">
-        <li>Learn Web Programming Basics</li>
-        <li>Learn Basic JavaScript Programming</li>
+        <li>Learn Web Programming Basics - Dicoding</li>
+        <li>Learn Basic JavaScript Programming - Dicoding</li>
       </ul>
     ),
   },
@@ -33,17 +31,17 @@ const AboutSection = () => {
   };
 
   return (
-    <section className="text-white" id="about">
+    <section className="text-white scroll-mt-20" id="about">
       <div className="gap-8 items-center py-8 xl:gap-16 ">
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
             Software Engineering graduate from Politeknik Negeri Bandung with a
             focus on front-end developer and data/business analysis. Skilled in
-            modern frameworks and databases including React.js, Next.js,
-            TypeScript, Vue.js, PostgreSQL, MySQL, and MongoDB. Highly motivated
-            to contribute to innovative projects by combining technical
-            expertise with analytical thinking.
+            modern frameworks and databases including React.js, Next.js, Vue.js,
+            TypeScript, PostgreSQL, MySQL, and MongoDB. Highly motivated to
+            contribute to innovative projects by combining technical expertise
+            with analytical thinking.
           </p>
           <div className="flex flex-row mt-8">
             <TabButton
