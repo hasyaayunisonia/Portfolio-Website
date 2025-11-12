ac58c492f9b20be56e1727871c34e4bb674be615
hasya <hasya.ayuni.tif20@polban.ac.id>
2025-11-12 21:28:37 +0700

    move pictures to assets folder

---
+++ src/app/assets/index.js
@@ -0,0 +1,83 @@
+import logo79 from "./experiences/79.png";
+import logoBims from "./experiences/bims.png";
+import BPI from "./projects/BPI.png";
+import ProdLoan from "./projects/ProdLoan.png";
+import Sijepe from "./projects/Sijepe.png";
+import FeedingData from "./projects/FeedingData.png";
+import KPPKL from "./projects/KPPKL.png";
+import Reengineering from "./projects/Reengineering.png";
+import KabarNikah from "./projects/KabarNikah.png";
+import TalentCenter from "./projects/TalentCenter.png";
+import Siap from "./projects/Siap.png";
+import GithubIcon from "./media/github-icon.svg";
+import LinkedInIcon from "./media/linkedin-icon.svg";
+import ReactLogo from "./technologies/reactLogo.png";
+import ViteLogo from "./technologies/viteLogo.png";
+import VueLogo from "./technologies/vueLogo.png";
+import NextJsLogo from "./technologies/nextjsLogo.jpg";
+import JavaScriptLogo from "./technologies/javascriptLogo.png";
+import TypeScriptLogo from "./technologies/typescriptLogo.png";
+import PostgresqlLogo from "./technologies/postgresqlLogo.png";
+import MongodbLogo from "./technologies/mongodbLogo.png";
+import MySqlLogo from "./technologies/mysqlLogo.png";
+import OracleLogo from "./technologies/oracleLogo.png";
+import JavaLogo from "./technologies/javaLogo.png";
+import KafkaLogo from "./technologies/kafkaLogo.png";
+import GitLogo from "./technologies/gitLogo.png";
+import DockerLogo from "./technologies/dockerLogo.png";
+import PhpLogo from "./technologies/phpLogo.png";
+import CodeIgniterLogo from "./technologies/codeigniterLogo.png";
+import SwaggerLogo from "./technologies/swaggerLogo.png";
+import MuiLogo from "./technologies/muiLogo.png";
+import AntdLogo from "./technologies/antdLogo.png";
+import ReduxLogo from "./technologies/reduxLogo.png";
+import myImage from "./images/me.png";
+
+const assets = {
+  experiences: {
+    logo79,
+    logoBims,
+  },
+  projects: {
+    BPI,
+    ProdLoan,
+    Sijepe,
+    FeedingData,
+    KPPKL,
+    Reengineering,
+    KabarNikah,
+    TalentCenter,
+    Siap,
+  },
+  media: {
+    GithubIcon,
+    LinkedInIcon,
+  },
+  technologies: {
+    ReactLogo,
+    ViteLogo,
+    VueLogo,
+    NextJsLogo,
+    JavaScriptLogo,
+    TypeScriptLogo,
+    PostgresqlLogo,
+    MongodbLogo,
+    MySqlLogo,
+    OracleLogo,
+    JavaLogo,
+    KafkaLogo,
+    GitLogo,
+    DockerLogo,
+    PhpLogo,
+    CodeIgniterLogo,
+    SwaggerLogo,
+    MuiLogo,
+    AntdLogo,
+    ReduxLogo,
+  },
+  images: {
+    myImage,
+  },
+};
+
+export default assets;
